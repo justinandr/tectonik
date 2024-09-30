1705ed9d5b735f574265c891f21b8628752bee79
Justin Rodriguez <justin.r@me.com>
2024-09-30 15:54:31 -0400

    switch to dev branch

--- app/(tabs)/settings.js
+++ app/(tabs)/settings.js
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function SettingsScreen() {
   return (
     <View>
-      <Text>Settings Screen</Text>
+      <Text>Settings Screens</Text>
     </View>
   );
 }
