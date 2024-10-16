4df77b7aab52135cebf42e79e3cd817a95e7316c
Justin <justin.r@me.com>
2024-10-15 21:05:14 -0400

    render select if multiple locations

--- app/(tabs)/new-ticket.tsx
+++ app/(tabs)/new-ticket.tsx
@@ -13,7 +13,8 @@ import {
   Label,
   Sheet,
   Adapt,
-  Button
+  Button,
+  View
 } from 'tamagui'
 
 type Location = {
@@ -25,6 +26,7 @@ export default function TabTwoScreen() {
 
   const [locations, setLocations] = useState<Location[]>([])
   const [locationNames, setLocationNames] = useState<string[]>([])
+  const [selectedLocation, setSelectedLocation] = useState('')
   const [userId, setUserId] = useState('')
   const [colorCode, setColorCode] = useState('green')
   const [title, setTitle] = useState('')
@@ -40,37 +42,47 @@ export default function TabTwoScreen() {
       }
 
       setUserId(user.id)
-      fetchUserLocations()
+      if (userId){
+        fetchUserLocations()
+      } else console.log('No user id')
+      if (locations){
       fetchLocationNames()
+      } else console.log('No locations')
     })
   }, [])
 
   async function fetchUserLocations() {
-    const { data, error } = await supabase
-      .from('users-locations')
-      .select('*')
-      .eq('user_id', userId)
+    if (userId) {
+      const { data, error } = await supabase
+        .from('users-locations')
+        .select('*')
+        .eq('user_id', userId)
 
-      if (error) {
-        console.error('Error fetching locations:', error)
-        return
-      }
+        if (error) {
+          console.error('Error fetching locations:', error)
+          return
+        }
 
-      setLocations(data)
+        setLocations(data)
+    }
   }
 
   async function fetchLocationNames() {
-    const { data, error } = await supabase
-      .from('locations')
-      .select('name')
-      .eq('id', locations.map(location => location.location_id))
+    if (locations) {
+      locations.map(async (location) => {
+        const { data, error } = await supabase
+          .from('locations')
+          .select('name')
+          .eq('id', location.location_id)
 
-      if (error) {
-        console.error('Error fetching location names:', error)
-        return
-      }
+          if (error) {
+            console.error('Error fetching location names:', error)
+            return
+          }
 
-      setLocationNames(data.map((location: { name: string }) => location.name))
+          setLocationNames(prev => [...prev, data[0].name])
+      } )
+    }
   }  
 
   async function submitForm() {
@@ -114,6 +126,8 @@ export default function TabTwoScreen() {
     }
   }, [formStatus])
 
+  console.log(locationNames)
+
   return (
     <ScrollView keyboardDismissMode='on-drag'>
       <Form 
@@ -130,6 +144,41 @@ export default function TabTwoScreen() {
         pb="$20"
       >
         <H2>New Ticket</H2>
+        {locationNames.length > 1 ?
+          <>
+            <Label htmlFor='location'>Location:</Label>
+            <Select value={locationNames[0]} onValueChange={setSelectedLocation} native id='location' defaultValue={locationNames[0]}>
+              <Select.Trigger iconAfter={ChevronDown}>
+                <Select.Value placeholder='Select One' />
+              </Select.Trigger>
+              <Adapt when="sm" platform="touch">
+                <Sheet>
+                  <Sheet.Frame>
+                    <Adapt.Contents />
+                  </Sheet.Frame>
+                  <Sheet.Overlay />
+                </Sheet>
+              </Adapt>
+
+              <Select.Content>
+                <Select.ScrollUpButton />
+                <Select.Viewport>
+                  <Select.Group>
+                    {locationNames.map((name, i) => (
+                      <Select.Item index={i} key={name} value={name}>
+                        <Select.ItemText>{name}</Select.ItemText>
+                        <Select.ItemIndicator marginLeft="auto">
+                          <Check size={16} />
+                        </Select.ItemIndicator>
+                      </Select.Item>
+                    ))}
+                  </Select.Group>
+                </Select.Viewport>
+                <Select.ScrollDownButton />
+              </Select.Content>
+            </Select>
+          </>
+        : null}
         <Label htmlFor='color_code'>Priority Level:</Label>
         <Select value={colorCode} onValueChange={setColorCode} native id='color_code' defaultValue={colorCode}>
           <Select.Trigger iconAfter={ChevronDown}>
@@ -167,9 +216,9 @@ export default function TabTwoScreen() {
           </Select.Content>
         </Select>
         <Label htmlFor='title'>Title</Label>
-        <Input id='title' value={title} onChangeText={text => setTitle(text)} placeholder='Enter title'/>
+        <Input value={title} onChangeText={text => setTitle(text)} placeholder='Enter title'/>
         <Label htmlFor='description'>Description:</Label>
-        <TextArea id='description' value={description} onChangeText={setDescription} height='$10' placeholder='Enter description'/>
+        <TextArea value={description} onChangeText={setDescription} height='$10' placeholder='Enter description'/>
         <Form.Trigger asChild disabled={formStatus !== 'off'}>
           <Button icon={formStatus === 'submitting' ? () => <Spinner /> : undefined}>Submit</Button>
         </Form.Trigger>
